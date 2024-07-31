f5ef8211fa592696a625048045a251fd6782cf03
sammajek <samtonmajek13@gmail.com>
2024-07-31 10:52:56 -0700

    Print

--- main.h
+++ main.h
@@ -3,6 +3,11 @@
 
 int _printf(const char *format, ...);
 int (*get_func(char c))(va_list);
-
+int handle_char(va_list args);
+int handle_string(va_list args);
+int handle_percent(va_list args);
+int handle_int(va_list args);
+void itoa(int value, char *str, int base);
+void reverse(char *str);
 
 #endif /* MAIN_H */
